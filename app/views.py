0e28f67d1da24c2eec656578ef606b0283b8c20f
gylmonteiro <gyl.tecno.especialista@gmail.com>
2022-10-17 11:53:27 -0300

    feat: created model Professor

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'app/home.html')
+from rest_framework.views import APIView
+from rest_framework.response import Response
+class HomeApiView(APIView):
+    def get(self, request, format=None):
+        return (Response({"nome": "Gyl Monteiro", "idade": 37}, status=200))
